eaefdc524213eea41c3e35c6c063ce2abdb7ca66
ak4zinn <112580354+ak4zinn@users.noreply.github.com>
2023-05-10 09:17:11 -0300

    10-05.c

--- Untitled-1.c
+++ Untitled-1.c
@@ -1,1 +1,17 @@
-cc ae2wg dsa basdaasdffffffafdfsssssaqweqrrrrrrrrrrrrvcxzv                    
+#include <stdio.h>
+int main() {
+char n;
+printf ("Se quiser instalar windows digite 1, caso queira linux digite 2:\n");
+printf("Digite umas das opções:\n");
+scanf("%s", &n);
+switch(n){
+    case '1':
+    printf("Intalar Windows");
+    break;
+    case '2':
+    printf("Instalar Linux");
+    break;
+    default:
+    printf("Opção inválida");
+}
+}
